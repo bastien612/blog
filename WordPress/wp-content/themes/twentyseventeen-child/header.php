77dcf42a6622957895442b118ba992ce815860c8
Netsab612 <bastien.meunier@yahoo.fr>
2020-03-08 15:46:42 +0100

    Modifie le header pour avoir le résultat basique souhaité par les bimers

--- WordPress/wp-content/themes/twentyseventeen-child/header.php
+++ WordPress/wp-content/themes/twentyseventeen-child/header.php
@@ -32,6 +32,7 @@
 
     <header id="masthead" class="site-header" role="banner">
 
+      <?php get_template_part( 'template-parts/header/header', 'image' ); ?>
       <?php if (has_nav_menu('top')) : ?>
         <div class="navigation-top site-navigation-fixed">
         <div class="wrap">
--- WordPress/wp-content/themes/twentyseventeen-child/template-parts/header/header-image.php
+++ WordPress/wp-content/themes/twentyseventeen-child/template-parts/header/header-image.php
@@ -12,19 +12,26 @@
 ?>
 <div class="custom-header">
 
-  <div class="custom-header-media">
-    <?php the_custom_header_markup(); ?>
-    <div class="call-to-action">
-      <h1>BIMERS</h1>
+
+  <!-- <div class="custom-header-media">-->
+    <?php //the_custom_header_markup(); ?>
+    <!--<div class="call-to-action">
+      <h1>BIMERS</h1> -->
       <!-- http://localhost:8080/wp-content/uploads/2020/02/cropped-01022020-adli-wahid-A87rz-MJN_E-unsplash-2.jpg -->
-      <!-- <div class='img-title'> -->
-      <!-- <img src="http://localhost:8080/wp-content/uploads/2020/02/bimers-police-blanc-png.png" onerror="console.log('fail')"> -->
-      <!-- </div> -->
-      <span>La communauté du BIM en Nouvelle-Aquitaine</span>
+      <!-- <div class='img-title'>
+      <img src="http://localhost:8080/wp-content/uploads/2020/02/bimers-police-blanc-png.png" onerror="console.log('fail')">
+      </div> -->
+      <!-- <span>La communauté du BIM en Nouvelle-Aquitaine</span>
       <div class="call-to-action-button">
         <button>S'inscrire</button>
       </div>
     </div>
+  </div> -->
+
+  <div class="custom-header-media">
+    <?php the_custom_header_markup(); ?>
   </div>
+
+  <?php //get_template_part('template-parts/header/site', 'branding'); ?>
 
 </div><!-- .custom-header -->
