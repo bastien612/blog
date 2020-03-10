f19596cc43549b61d4c56b0ef64a85a5aa46b2d8
Netsab612 <bastien.meunier@yahoo.fr>
2020-03-10 23:52:45 +0100

    Add algorythm to make pieces levitate + add link to picture for real website

--- WordPress/wp-content/themes/twentyseventeen-child/assets/js/custom.js
+++ WordPress/wp-content/themes/twentyseventeen-child/assets/js/custom.js
@@ -22,18 +22,78 @@
 
 function animatePieces($) {
   console.log("Animation !!!");
-  console.log("GSAP ? :", gsap);
-  const tl = gsap.timeline();
-  const piece1 = $(".piece1").first();
-  const xLeft = piece1.position().left;
-  console.log("piece1 : ", piece1);
-  console.log("xLeft : ", xLeft);
-  tl.to(".piece1", { x: xLeft + 100, duration: 5, ease: "power4" });
+
+  initTimeLine(".piece1");
+  initTimeLine(".piece2");
+  initTimeLine(".piece3");
+  initTimeLine(".piece4");
+  initTimeLine(".piece5");
+}
+
+function initTimeLine(piece) {
+  const tl = gsap.timeline({ repeat: -1, delay: 0 });
+  let sign = Math.random() < 0.5;
+  console.log("pouet : ", sign);
+
+  let i;
+  for (i = 0; i < 8; i++) {
+    console.log("Add move :", sign);
+    console.log("i: ", i);
+    sign ? addMoveUp(tl, piece) : addMoveDown(tl, piece);
+    sign = !sign;
+  }
+  addMoveToStartingPlace(tl, piece);
 
   tl.play();
 }
 
-function movePiece($, pieceName) {
-  const piece = $(pieceName);
-  console.log("GSAP ? :", gsap);
+function addMoveToStartingPlace(timeline, name) {
+  timeline.to(name, {
+    x: 0,
+    y: 0,
+    duration: 6,
+    ease: "power1.inOut"
+  });
+}
+
+function addMoveUp(timeline, name) {
+  addMove(timeline, name, false);
+}
+
+function addMoveDown(timeline, name) {
+  addMove(timeline, name, true);
+}
+
+function addMove(timeline, name, ySign) {
+  const x = calcNewX();
+  const y = calcNewY();
+  const duration = getRandBetween(5, 8);
+  // console.log(`x: ${x}, y: ${y} , duration: ${duration}`);
+
+  timeline.to(name, {
+    x: x,
+    y: ySign ? y : -y,
+    duration: duration,
+    ease: "power1.inOut"
+  });
+}
+
+function calcNewX() {
+  return calcNewCoord(5, 10);
+}
+
+function calcNewY() {
+  return getRandBetween(10, 15);
+}
+
+function calcNewCoord(min, max) {
+  const sign = Math.random();
+  const value = getRandBetween(min, max);
+  return sign < 0.5 ? value : -value;
+}
+
+function getRandBetween(min, max) {
+  const amplitude = max - min;
+  const value = Math.floor(Math.random() * amplitude);
+  return min + value;
 }
